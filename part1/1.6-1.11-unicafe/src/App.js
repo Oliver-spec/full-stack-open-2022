972e423b235125524dc8947fe90ecdab3dd78fc5
Oliver <1155175373@link.cuhk.edu.hk>
2022-05-31 15:20:45 +0800

    finish unicafe

--- part1/1.6-1.11-unicafe/src/App.js
+++ part1/1.6-1.11-unicafe/src/App.js
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+// compontents are here
+const Button = ({text, onClick}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const StatisticsLine = ({text, value}) => (
+  <tr>
+    <td>
+        {text} {value}
+    </td>
+  </tr>
+)
+
 const Statistics = ({good, neutral, bad, all, avg, positive}) => {
   if (good === 0 && neutral === 0 && bad === 0) {
     return (
@@ -9,26 +24,32 @@ const Statistics = ({good, neutral, bad, all, avg, positive}) => {
     )
   } else {
     return (
-      <>
-        <p>
-          good {good}
-        </p>
-        <p>
-          neutral {neutral}
-        </p>
-        <p>
-          bad {bad}
-        </p>
-        <p>
-          all {all}
-        </p>
-        <p>
-          average {avg}
-        </p>
-        <p>
-          positive {positive}%
-        </p>
-      </>
+      <table>
+        <StatisticsLine
+          text='good'
+          value={good}
+        />
+        <StatisticsLine
+          text='neutral'
+          value={neutral}
+        />
+        <StatisticsLine
+          text='bad'
+          value={bad}
+        />
+        <StatisticsLine
+          text='all'
+          value={all}
+        />
+        <StatisticsLine
+          text='avg'
+          value={avg}
+        />
+        <StatisticsLine
+          text='positive'
+          value={positive + ' %'}
+        />
+      </table>
     )
   }
 }
@@ -50,15 +71,18 @@ const App = () => {
       <h1>
         give feedback
       </h1>
-      <button onClick={rateGood}>
-        good
-      </button>
-      <button onClick={rateNeutral}>
-        neutral
-      </button>
-      <button onClick={rateBad}>
-        bad
-      </button>
+      <Button
+        text='good'
+        onClick={rateGood}
+      />
+      <Button
+        text='neutral'
+        onClick={rateNeutral}
+      />
+      <Button
+        text='bad'
+        onClick={rateBad}
+      />
       <h1>
         statistics
       </h1>
